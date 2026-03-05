5601830347b7530afde8dab54961953fa6f2baa2
Chandrika Vishwakarma <vickyopklopkl@gmail.com>
2026-03-05 23:37:54 +0530

    Two Sum (input sorted array)

---
+++ Array/twoSum_SortedInput.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& numbers, int target) {
+    
+    int i = 0, j = numbers.size() - 1;
+
+    while(i < j){
+        if(numbers[i] + numbers[j] == target)
+            return {i+1, j+1};
+        else if(numbers[i] + numbers[j] < target)
+            i++;
+        else
+            j--;
+    }
+
+    return {};
+}
+
+int main(){
+
+    vector<int> numbers = {2,7,11,15};
+    int target = 9;
+
+    vector<int> result = twoSum(numbers, target);
+
+    cout << result[0] << " " << result[1];
+
+}
